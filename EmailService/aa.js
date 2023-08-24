4ff10eafc43b5bd5b46133cde878b3d3708eb1c8
dudu <dudvil1@gmail.com>
2023-08-24 09:12:34 +0300

    11
    11

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -7,47 +7,61 @@ const connSettings = {
   password: "your_password",
 };
 
-const conn = new Client();
+const executeCommands = async () => {
+  const conn = new Client();
 
-conn.on("ready", () => {
-  console.log("Connected");
+  try {
+    await new Promise((resolve, reject) => {
+      conn.on("ready", () => {
+        console.log("Connected");
 
-  conn.shell((err, stream) => {
-    if (err) throw err;
+        conn.shell((err, stream) => {
+          if (err) reject(err);
 
-    let commandIndex = 0;
+          let commandIndex = 0;
 
-    const commands = [
-      { pattern: /[$#>] $/, command: "cd test" },
-      { pattern: /[$#>] $/, command: `git clone your_git_repository_url` },
-      { pattern: /Username for .*: $/, command: "user123" },
-      { pattern: /Password for .*: $/, command: "pass123" },
-      // Add more commands as needed
-    ];
+          const commands = [
+            { pattern: /[$#>] $/, command: "cd test" },
+            {
+              pattern: /[$#>] $/,
+              command: `git clone your_git_repository_url`,
+            },
+            { pattern: /Username for .*: $/, command: "user123" },
+            { pattern: /Password for .*: $/, command: "pass123" },
+            // Add more commands as needed
+          ];
 
-    stream.on("data", (data) => {
-      const output = data.toString();
-      console.log(output);
+          stream.on("data", (data) => {
+            const output = data.toString();
+            console.log(output);
 
-      const currentCommand = commands[commandIndex];
-      if (currentCommand && currentCommand.pattern.test(output)) {
-        stream.write(currentCommand.command + "\n");
-        commandIndex++;
-      } else if (commandIndex >= commands.length) {
-        console.log("All commands sent");
-        stream.end("exit\n");
-      }
+            const currentCommand = commands[commandIndex];
+            if (currentCommand && currentCommand.pattern.test(output)) {
+              stream.write(currentCommand.command + "\n");
+              commandIndex++;
+            } else if (commandIndex >= commands.length) {
+              console.log("All commands sent");
+              stream.end("exit\n");
+            }
+          });
+
+          stream.on("close", () => {
+            console.log("Stream closed");
+            conn.end();
+            resolve(); // Resolve the promise when the stream is closed
+          });
+        });
+      });
+
+      conn.connect(connSettings);
+
+      conn.on("error", (err) => {
+        reject(err);
+      });
     });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
 
-    stream.on("close", () => {
-      console.log("Stream closed");
-      conn.end();
-    });
-  });
-});
-
-conn.connect(connSettings);
-
-conn.on("error", (err) => {
-  console.error("Error:", err.message);
-});
+executeCommands();
