fec2bedbf142fccadd351e7d833ca04f420e04ed
dudu <dudvil1@gmail.com>
2023-09-04 17:02:36 +0300

    eeee
    jhgvhjgh

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -1,61 +1,43 @@
-const { Client } = require("ssh2");
+const Client = require("ssh2").Client;
 
-const conn = new Client();
+function executeDockerPsOverSSH(sshConfig) {
+  return new Promise(async (resolve, reject) => {
+    const conn = new Client();
 
-conn
-  .on("ready", () => {
-    console.log("SSH connection ready");
-
-    // Run the docker ps command on the remote server
-    conn.shell((err, stream) => {
-      if (err) throw err;
-
-      stream
-        .on("close", () => {
-          console.log("SSH shell closed");
-          conn.end();
-        })
-        .on("data", (data) => {
-          const dataString = data.toString();
-          const lines = dataString.trim().split("\n");
-
-          lines.forEach((line) => {
-            try {
-              const container = JSON.parse(line);
-              console.log(container);
-            } catch (error) {
-              console.error("Error parsing JSON:", error);
+    try {
+      await conn
+        .on("ready", function () {
+          conn.exec("docker ps", function (err, stream) {
+            if (err) {
+              conn.end();
+              reject(err);
+              return;
             }
+
+            let output = "";
+
+            stream
+              .on("data", function (data) {
+                output += data.toString();
+              })
+              .on("end", function () {
+                conn.end();
+                resolve(output);
+              })
+              .stderr.on("data", function (data) {
+                conn.end();
+                reject(new Error(data.toString()));
+              });
           });
-        });
-
-      stream.end(
-        "docker ps --format '{{json .}}' | grep \"0.0.0.0:3306->3306/tcp\"\nexit\n"
-      );
-    });
-  })
-  .connect({
-    host: "your_remote_server_ip",
-    port: 22,
-    username: "your_username",
-    password: "your_password",
+        })
+        .connect(sshConfig);
+    } catch (error) {
+      reject(error);
+    }
   });
-
-
-  const createdAtPattern = /"CreatedAt":\s*"(.*?)"/;
-const createdAtMatch = jsonString.match(createdAtPattern);
-
-if (createdAtMatch) {
-  const createdAtValue = createdAtMatch[1].replace(/\s+\w+$/, ''); // Remove the timezone abbreviation
-  const correctedJsonString = jsonString.replace(createdAtPattern, `"CreatedAt": "${createdAtValue}"`);
-
-  try {
-    const jsonObject = JSON.parse(correctedJsonString);
-    console.log(jsonObject);
-  } catch (error) {
-    console.error('Error parsing JSON:', error);
-  }
-} else {
-  console.error('CreatedAt value not found in the JSON string.');
 }
-const idPattern = /"ID":\s*"([^"]+)"/;
+
+module.exports = executeDockerPsOverSSH;
+
+const result = await executeDockerPsOverSSH(sshConfig);
+console.log("Docker PS Output:", result);
