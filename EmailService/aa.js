94371f9630e46519d14b4f7641a636258e47d6fd
dudu <dudvil1@gmail.com>
2023-09-18 17:21:23 +0300

    aaasasa
    adsd

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -1,19 +1,30 @@
 const fs = require("fs");
 
-// Read the text file
-const fileContents = fs.readFileSync("your_file.txt", "utf8");
+// Define the input file path
+const inputFilePath = "your_input.txt";
 
-const startPhrase = "docker build";
-const endPhrase = "docker run";
+// Read the contents of the file
+fs.readFile(inputFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-const startIndex = fileContents.indexOf(startPhrase);
-const endIndex = fileContents.indexOf(endPhrase);
+  // Use regular expressions to extract text between "docker build" and "docker run"
+  const regex = /docker build([\s\S]*?)docker run/g;
+  const matches = [...data.matchAll(regex)];
 
-if (startIndex !== -1 && endIndex !== -1) {
-  const extractedText = fileContents
-    .substring(startIndex + startPhrase.length, endIndex)
-    .trim();
-  console.log(extractedText);
-} else {
-  console.log("Start and/or end phrases not found in the file.");
-}
+  // Extracted text will be stored in this array
+  const extractedText = [];
+
+  // Iterate over the matches and extract the content
+  for (const match of matches) {
+    extractedText.push(match[1].trim());
+  }
+
+  // Join the extracted text into a single string (if needed)
+  const finalText = extractedText.join("\n");
+
+  // Print or process the final text
+  console.log(finalText);
+});
