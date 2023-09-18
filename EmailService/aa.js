14aee7bd728513659759bf2ac853466b843e7e2c
dudu <dudvil1@gmail.com>
2023-09-18 16:35:49 +0300

    fs
    ddd

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -1,7 +1,19 @@
-function getSliceOfArray(arr, index) {
-  const sliceSize = 10;
-  const startIndex = Math.max(arr.length - index * sliceSize, 0);
-  const endIndex = Math.min(startIndex + sliceSize, arr.length);
+const fs = require("fs");
 
-  return arr.slice(startIndex, endIndex);
+// Read the text file
+const fileContents = fs.readFileSync("your_file.txt", "utf8");
+
+const startPhrase = "docker build";
+const endPhrase = "docker run";
+
+const startIndex = fileContents.indexOf(startPhrase);
+const endIndex = fileContents.indexOf(endPhrase);
+
+if (startIndex !== -1 && endIndex !== -1) {
+  const extractedText = fileContents
+    .substring(startIndex + startPhrase.length, endIndex)
+    .trim();
+  console.log(extractedText);
+} else {
+  console.log("Start and/or end phrases not found in the file.");
 }
