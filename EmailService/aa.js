03e607f4db834cd97f0d580a53594c61149a173b
dudu <dudvil1@gmail.com>
2023-09-26 11:31:24 +0300

    sswr
    2323

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -1,18 +1,45 @@
-function extractDockerBuildRunText(text) {
-  // Split the text into an array of lines.
-  const lines = text.split("\n");
+const fs = require("fs");
+const path = require("path");
 
-  // Find the index of the first line that contains "docker build".
-  const buildIndex = lines.findIndex((line) => line.includes("docker build"));
+function findPortsInFolders(baseDir, targetPort) {
+  const ports = [];
 
-  // Find the index of the first line that contains "docker run" after the index of the "docker build" line.
-  const runIndex = lines.findIndex(
-    (line, index) => line.includes("docker run") && index > buildIndex
-  );
+  function processFolder(folderPath) {
+    const files = fs.readdirSync(folderPath);
 
-  // Extract the lines from the array between the two indices.
-  const extractedLines = lines.slice(buildIndex + 1, runIndex);
+    if (files.includes("package.json")) {
+      const packageJsonPath = path.join(folderPath, "package.json");
+      const packageJson = require(packageJsonPath); // You can use require to read package.json
 
-  // Return the extracted lines as a string.
-  return extractedLines.join("\n");
+      if (packageJson && packageJson.port) {
+        ports.push(packageJson.port);
+      }
+    }
+
+    // Recursively process subfolders
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        processFolder(filePath);
+      }
+    }
+  }
+
+  processFolder(baseDir);
+
+  // Check if the targetPort is included in the collected ports
+  const portExists = ports.includes(targetPort);
+
+  return portExists;
+}
+
+// Example usage:
+const baseDirectory = "/path/to/your/directory";
+const targetPort = 3000; // Replace with the port you want to check
+const result = findPortsInFolders(baseDirectory, targetPort);
+
+if (result) {
+  console.log(`Port ${targetPort} is found in the package.json files.`);
+} else {
+  console.log(`Port ${targetPort} is not found in the package.json files.`);
 }
