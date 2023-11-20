dc3b1cccd68d19cc41ea810937eb2308a411d75c
dudu <dudvil1@gmail.com>
2023-11-20 09:24:31 +0200

    dd
    jhghlj

--- EmailService/aa.js
+++ EmailService/aa.js
@@ -1,45 +1,86 @@
-const fs = require("fs");
-const path = require("path");
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 
-function findPortsInFolders(baseDir, targetPort) {
-  const ports = [];
+public class JsonValidator
+{
+    public static T DeserializeAndValidate<T>(string jsonString, params string[] fieldNames)
+    {
+        try
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Converters = { new CustomDateTimeConverter(fieldNames) },
+                // Add other settings if needed
+            };
 
-  function processFolder(folderPath) {
-    const files = fs.readdirSync(folderPath);
-
-    if (files.includes("package.json")) {
-      const packageJsonPath = path.join(folderPath, "package.json");
-      const packageJson = require(packageJsonPath); // You can use require to read package.json
-
-      if (packageJson && packageJson.port) {
-        ports.push(packageJson.port);
-      }
+            return JsonConvert.DeserializeObject<T>(jsonString, settings);
+        }
+        catch (JsonSerializationException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            // Handle the error as needed
+            return default(T); // Or throw an exception, return a specific value, etc.
+        }
     }
-
-    // Recursively process subfolders
-    for (const file of files) {
-      const filePath = path.join(folderPath, file);
-      if (fs.statSync(filePath).isDirectory()) {
-        processFolder(filePath);
-      }
-    }
-  }
-
-  processFolder(baseDir);
-
-  // Check if the targetPort is included in the collected ports
-  const portExists = ports.includes(targetPort);
-
-  return portExists;
 }
 
-// Example usage:
-const baseDirectory = "/path/to/your/directory";
-const targetPort = 3000; // Replace with the port you want to check
-const result = findPortsInFolders(baseDirectory, targetPort);
+public class CustomDateTimeConverter : JsonConverter<DateTime>
+{
+    private readonly string[] fieldNames;
 
-if (result) {
-  console.log(`Port ${targetPort} is found in the package.json files.`);
-} else {
-  console.log(`Port ${targetPort} is not found in the package.json files.`);
+    public CustomDateTimeConverter(string[] fieldNames)
+    {
+        this.fieldNames = fieldNames;
+    }
+
+    public override DateTime ReadJson(JsonReader reader, Type objectType, DateTime existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Float || reader.TokenType == JsonToken.Integer)
+        {
+            throw new JsonSerializationException($"Invalid date format for field(s) '{string.Join(", ", fieldNames)}'. Expected string but got {reader.Value}");
+        }
+
+        if (reader.TokenType == JsonToken.String)
+        {
+            if (DateTime.TryParse(reader.Value.ToString(), out DateTime result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new JsonSerializationException($"Invalid date format for field(s) '{string.Join(", ", fieldNames)}'. Unable to parse: {reader.Value}");
+            }
+        }
+
+        throw new JsonSerializationException($"Unexpected token type: {reader.TokenType}");
+    }
+
+    public override void WriteJson(JsonWriter writer, DateTime value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class YourClass
+{
+    public DateTime YourDateTimeField { get; set; }
+
+    // Other fields in YourClass
+}
+
+class Program
+{
+    static void Main()
+    {
+        string jsonString = "{ \"YourDateTimeField\": \"1.5\", \"OtherField\": \"someValue\" }";
+
+        YourClass deserializedObject = JsonValidator.DeserializeAndValidate<YourClass>(jsonString, "YourDateTimeField", "OtherField");
+
+        if (deserializedObject != null)
+        {
+            Console.WriteLine(deserializedObject.YourDateTimeField);
+        }
+    }
 }
