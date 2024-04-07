40efb49f560c6074ba124b0473ef255bd9a38cdb
dudu <dudvil1@gmail.com>
2024-04-07 15:28:03 +0300

    a
    a

--- EmailService/Service/dd.js
+++ EmailService/Service/dd.js
@@ -1,45 +1,58 @@
 // Function to generate XLSX content
 function generateXLSXContent() {
-    var content = '<?xml version="1.0"?>\n';
-    content += '<?mso-application progid="Excel.Sheet"?>\n';
-    content += '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40">\n';
-    
-    content += ' <Worksheet ss:Name="Sheet1">\n';
-    content += '  <Table>\n';
+  var content = '<?xml version="1.0"?>\n';
+  content += '<?mso-application progid="Excel.Sheet"?>\n';
+  content +=
+    '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40">\n';
 
-    content += '   <Row>\n';
-    content += '    <Cell><Data ss:Type="String">Test1</Data></Cell>\n';
-    content += '    <Cell><Data ss:Type="String">Test2</Data></Cell>\n';
-    content += '    <Cell><Data ss:Type="String">Test3</Data></Cell>\n';
-    content += '   </Row>\n';
+  content += ' <Worksheet ss:Name="Sheet1">\n';
+  content += "  <Table>\n";
 
-    content += '   <Row>\n';
-    content += '    <Cell><Data ss:Type="String">Value1</Data></Cell>\n';
-    content += '    <Cell><Data ss:Type="String">Value2</Data></Cell>\n';
-    content += '    <Cell><Data ss:Type="String">Value3</Data></Cell>\n';
-    content += '   </Row>\n';
+  content += "   <Row>\n";
+  content += '    <Cell><Data ss:Type="String">Test1</Data></Cell>\n';
+  content += '    <Cell><Data ss:Type="String">Test2</Data></Cell>\n';
+  content += '    <Cell><Data ss:Type="String">Test3</Data></Cell>\n';
+  content += "   </Row>\n";
 
-    content += '  </Table>\n';
-    content += ' </Worksheet>\n';
+  content += "   <Row>\n";
+  content += '    <Cell><Data ss:Type="String">Value1</Data></Cell>\n';
+  content += '    <Cell><Data ss:Type="String">Value2</Data></Cell>\n';
+  content += '    <Cell><Data ss:Type="String">Value3</Data></Cell>\n';
+  content += "   </Row>\n";
 
-    content += '</Workbook>';
+  content += "  </Table>\n";
+  content += " </Worksheet>\n";
 
-    return content;
+  content += "</Workbook>";
+
+  return content;
 }
 
 // Function to download file
 function downloadFile(blob, filename) {
-    var a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = filename;
-    a.click();
+  var a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
 }
 
-// Event listener for button click
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('generateBtn').addEventListener('click', function() {
-        var data = generateXLSXContent();
-        var blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-        downloadFile(blob, 'example.xlsx');
+// Function to create button
+function createButton() {
+  var button = document.createElement("button");
+  button.textContent = "Generate XLSX";
+  button.id = "generateBtn";
+  button.addEventListener("click", function () {
+    var data = generateXLSXContent();
+    var blob = new Blob([data], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     });
+    downloadFile(blob, "example.xlsx");
+  });
+  return button;
+}
+
+// Add the button to the body when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  var button = createButton();
+  document.body.appendChild(button);
 });
